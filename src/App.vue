a8ad5c64ec3d0b8eb85da464b657ca19ccafb4d0
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-04-22 14:01:43 -0300

    added results box

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,40 @@
   <h1>Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">Start</button>
   <!-- bind is necessary due to the fact that I'm using a variable to pass on a value -->
-  <Block v-if="isPlaying" :delay="delay"/>
+  <Block v-if="isPlaying" :delay="delay" @finish="endGame"/>
+	<Results v-if="showResults" :score="score"/>
 </template>
 
 <script>
 import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
   components: {
     Block,
+    Results,
   },
   data() {
     return {
       isPlaying: false,
       delay: null,
+			score: null,
+			showResults: false,
     }
   },
   methods: {
     start() {
       //generates a random time between 1 and 5 seconds
       this.delay = 1000 + Math.random() * 4000
+			this.showResults = false
       this.isPlaying = true
-    }
+    },
+		endGame(reactionTime) {
+			this.isPlaying = false
+			this.showResults = true
+			this.score = reactionTime
+		}
   }
 }
 </script>
---
+++ src/components/Results.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="results">
+		Reaction time: {{ score }} ms
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['score'],
+}
+</script>
+
+<style>
+    .results {
+        width: 400px;
+        border-radius: 20px;
+        background: #0faf87;
+        color: #fff;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+</style>
